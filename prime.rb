3432917764bedf5eb7a296560129c454ee297bec
bennybop <bennybop74@yahoo.com>
2017-11-30 01:47:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,9 @@ def prime?(num)
   x = (2..num)
   if num <= 0 || num == 1
     return false
-  elsif numbers.any? { |i| num > i && num % i == 0}
+  elsif x.any? { |i| num > i && num % i == 0}
     return false
   else
     return true
   end
 end
-
-    
-  
